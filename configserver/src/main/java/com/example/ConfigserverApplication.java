85b473f2ac489f3a91bc1ab39455f624f138da1a
ravishanker3188 <ravishankermr@gmail.com>
2016-09-03 14:09:04 +0530

    Config Server Intialized

--- configserver/src/main/java/com/example/ConfigserverApplication.java
+++ configserver/src/main/java/com/example/ConfigserverApplication.java
@@ -2,13 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class DemoApplication {
+public class ConfigserverApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ConfigserverApplication.class, args);
 	}
 }
