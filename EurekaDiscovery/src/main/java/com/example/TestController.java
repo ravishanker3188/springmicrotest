1e758bf3b5d6e2e583bf04a9086774b4d5fad738
ravishanker3188 <ravishankermr@gmail.com>
2016-09-03 17:16:38 +0530

    Started EurekaClient,Server and Config Server

--- EurekaDiscovery/src/main/java/com/example/TestController.java
+++ EurekaDiscovery/src/main/java/com/example/TestController.java
@@ -1,7 +1,19 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by Ravi on 9/3/2016.
  */
+@RestController
 public class TestController {
+    @Value("${testname}")
+    private String testName;
+
+    @RequestMapping("/test")
+    public String showLuckyWord() {
+        return "The lucky word is: " + testName;
+    }
 }
